eebb7bebe71ac3b782e41f47b322dc1b09e8220e
Jack Wotta <jwotta2@huskers.unl.edu>
2021-08-27 01:28:25 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Jack Wotta, no partner
+ *         jwotta2@huskers.unl.edu
+ * Date: 2021/08/24
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Jack Wotta\n");
+
+  return 0;
+}
